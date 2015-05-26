7e47eb29bc74798ea768d84dc89d163960c0c984
Kenneth To <kennethkto@gmail.com>
2015-05-26 13:02:46 -0600

    move google map function code from application layouts to main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,27 @@ $(function(){
 
   console.log("test javascript");
 
+  function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(39.7392, -104.9903),
+      zoom: 4,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  function initialize2() {
+    var mapCanvas = document.getElementById('map-canvas-2');
+    var mapOptions = {
+      center: new google.maps.LatLng(40.7127, -74.0059),
+      zoom: 4,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize2);
+
+
 });
