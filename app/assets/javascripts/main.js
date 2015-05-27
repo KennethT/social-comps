f2fd826de10da089babe7d1f03f9edcae8669de9
Kenneth To <kennethkto@gmail.com>
2015-05-27 13:35:22 -0600

    add access to heatmap visualization library for google maps

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,16 +15,25 @@ $(function(){
 
   // default sections hidden
   $(".tweet_both").hide();
+  $(".sentiment_both").hide();
 
   //Three Primary Buttons - Hiding and Showing
   $(".twt_btn").on('click', function(){
         $(".map_both").hide();
+        $(".sentiment_both").hide();
         $(".tweet_both").show();
   });
 
   $(".map_btn").on('click', function(){
         $(".tweet_both").hide();
+        $(".sentiment_both").hide();
         $(".map_both").show();
+  });
+
+  $(".sen_btn").on('click', function(){
+        $(".tweet_both").hide();
+        $(".map_both").hide();
+        $(".sentiment_both").show();
   });
 
 });
