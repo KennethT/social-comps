27b1414cbaa2a889452da1918dadff18db1c67e7
Kenneth To <kennethkto@gmail.com>
2015-05-27 09:36:31 -0600

    move tweet rendering logic to partials

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,23 @@ class WelcomeController < ApplicationController
   def index
     if params[:search_term1].present?
       @tweet1 = $twitter.search("#{params[:search_term1]}", result_type: "recent", lang: "en", geo_enabled: "true").take(10)
+
+      @geo1 = @tweet1.map do |tweet|
+        [tweet.geo.lat, tweet.geo.long].flatten
+      end.reject {|array| array == [] || array == [0.0, 0.0] }
+
     end
 
     if params[:search_term2].present?
       @tweet2 = $twitter.search("#{params[:search_term2]}", result_type: "recent", lang: "en", geo_enabled: "true").take(10)
+
+
+      @geo2 = @tweet2.map do |tweet|
+        [tweet.geo.lat, tweet.geo.long].flatten
+      end.reject {|array| array == [] || array == [0.0, 0.0] }
+
     end
+
   end
 
 
