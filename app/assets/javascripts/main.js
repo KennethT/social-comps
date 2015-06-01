22c17146486dab94b604ccce9003586a42c6e7e9
Kenneth To <kennethkto@gmail.com>
2015-06-01 11:49:33 -0600

    refactor button selection code into a function that takes arguments

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,22 +5,16 @@ $(function(){
   $(".sentiment_both").hide();
 
   //Three Primary Buttons - Hiding and Showing
-  $(".twt_btn").on('click', function(){
-        $(".map_both").hide();
-        $(".sentiment_both").hide();
-        $(".tweet_both").show();
-  });
+  function buttonSelect(cssElement, selectedItem, deselected1, deselected2) {
+    $(cssElement).on('click', function(){
+      $(selectedItem).show();
+      $(deselected1).hide();
+      $(deselected2).hide();
+    });
+  }
 
-  $(".map_btn").on('click', function(){
-        $(".tweet_both").hide();
-        $(".sentiment_both").hide();
-        $(".map_both").show();
-  });
-
-  $(".sen_btn").on('click', function(){
-        $(".tweet_both").hide();
-        $(".map_both").hide();
-        $(".sentiment_both").show();
-  });
+  buttonSelect(".twt_btn", ".tweet_both", ".map_both", ".sentiment_both");
+  buttonSelect(".map_btn", ".map_both", ".tweet_both", ".sentiment_both");
+  buttonSelect(".sen_btn", ".sentiment_both", ".tweet_both", ".map_both");
 
 });
