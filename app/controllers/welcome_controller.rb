a64a5c0e00a742fe31cc08a0c017c66e844423b7
Kenneth To <kennethkto@gmail.com>
2015-06-05 12:56:30 -0600

    add aggregation of tweets to be sent to watson personality insights api/micro service

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,12 @@ class WelcomeController < ApplicationController
       @geo1 = @tweet1.map do |tweet|
         [tweet.geo.lat, tweet.geo.long].flatten
       end.reject {|array| array == [] || array == [0.0, 0.0] }
+
+      # creating an element to be sent to bluemix watson api
+      @watson1 = @tweet1.map do |tweet|
+        tweet.text
+      end.join(', ').to_json
+
     end
 
     if params[:search_term2].present?
@@ -18,6 +24,19 @@ class WelcomeController < ApplicationController
       end.reject {|array| array == [] || array == [0.0, 0.0] }
 
     end
+
+    # This is the Sentiment Analysis portion
+
+    # @tweet1.each do |tweet|
+    #    @emotion = SadPanda.emotion(tweet.text.dup).capitalize
+    #    Tweet.create(emotion: @emotion, text: tweet.text, screen_name: tweet.user.screen_name)
+    #  end
+    #
+    #  @emotions = @tweet1.map do |tweet|
+    #    SadPanda.emotion(tweet.text.dup).capitalize
+    #  end.uniq
+    #
+    #  @emotions.delete_if {|i| i == "Ambiguous"}
 
   end
 
